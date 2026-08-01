65b0a342bd751b290dd5d439ff99abcdb79ccc33
Ahkar Hein <ahkarhein04@gmail.com>
2026-07-31 21:48:17 -0700

    add randomized temp reading to simulate sensor data

--- device/main.cpp
+++ device/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
     std::cout << "Device starting up..." << std::endl;
 
     while (true) {
-        std::cout << "Reading Sensor..." << std::endl;
+        float temperature = 20.0 + (std::rand() % 100) / 10.0;
+        std::cout << "Temperature: " << temperature << " C" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     return 0;
