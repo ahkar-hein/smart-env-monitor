e0425913c4f5deb35b3068f7f9838452232d1a21
Ahkar Hein <ahkarhein04@gmail.com>
2026-07-31 21:34:11 -0700

    add loop with 1 sec delay to simulate sensor sampling

--- device/main.cpp
+++ device/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+
 int main() {
     std::cout << "Device starting up..." << std::endl;
+
+    while (true) {
+        std::cout << "Reading Sensor..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
     return 0;
 }
