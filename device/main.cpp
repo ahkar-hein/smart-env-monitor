603bf025837f42c7076d1b50293f017afa949af2
Ahkar Hein <ahkarhein04@gmail.com>
2026-07-31 22:19:09 -0700

    Add state machine (Normal/Warning/Alarm) based on temp

--- device/main.cpp
+++ device/main.cpp
@@ -3,13 +3,43 @@
 #include <thread>
 #include <cstdlib>
 #include <ctime>
+#include <string>
+
+enum State {
+    NORMAL,
+    WARNING,
+    ALARM
+};
+
+std::string stateToString(State s) {
+    if (s == NORMAL) return "NORMAL";
+    if (s == WARNING) return "WARNING";
+    if (s == ALARM) return "ALARM"  ;
+    return "UNKNOW";
+}
 
 int main() {
     std::cout << "Device starting up..." << std::endl;
 
     while (true) {
         float temperature = 20.0 + (std::rand() % 100) / 10.0;
-        std::cout << "Temperature: " << temperature << " C" << std::endl;
+
+        State currentState;
+        if (temperature >= 28.00)
+        {
+            currentState = ALARM;
+        }
+        else if (temperature >= 25.00)
+        {
+            currentState = WARNING;
+        }
+        else
+        {
+            currentState = NORMAL;
+        }
+        
+        
+        std::cout << "Temperature: " << temperature << " C | State: " << stateToString(currentState) << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     return 0;
